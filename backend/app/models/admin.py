261ddfc7af7ba96684f9011b1236a3a56489b544
Shreyash-Arya06 <shreyash.arya.iit.bhu@gmail.com>
2026-03-03 22:20:52 +0530

    Updated admin schemas and fixed typos in admin model

--- backend/app/models/admin.py
+++ backend/app/models/admin.py
@@ -6,12 +6,11 @@ from sqlalchemy import Column, DateTime, func
 class Admin(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     email: str = Field(index=True, unique=True)
-    hashed_password: str | None = Field(default=None)
+    password: str | None = Field(default=None)
     auth_provider: str = Field(default="local")
-
     name: str = Field()
-    pose_line_1: str = Field()
-    pose_line_2: str | None = Field(default=None)
+    pos_line_1: str = Field()
+    pos_line_2: str | None = Field(default=None)
     orgs_name: str = Field()
     about: str | None = Field(default=None)
     resume_url: str | None = Field(default=None)
--- backend/app/schemas/admin.py
+++ backend/app/schemas/admin.py
@@ -1,12 +1,42 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 class Token(BaseModel):
     access_token: str
     token_type: str
 
 class TokenData(BaseModel):
-    username: str | None = None
+    email: str | None = None
 
 class AdminCreate(BaseModel):
-    username: str
+    email: EmailStr
     password: str
+    name: str
+    pos_line_1: str
+    pos_line_2: str | None = None
+    orgs_name: str
+    about: str | None = None
+    resume_url: str | None = None
+
+class AdminUpdate(BaseModel):
+    name: str | None = None
+    pos_line_1: str | None = None
+    pos_line_2: str | None = None
+    orgs_name: str | None = None
+    about: str | None = None
+
+class AdminChangeCredentials(BaseModel):
+    email: EmailStr | None = None
+    password: str | None = None
+
+class AdminUpdateResume(BaseModel):
+    resume_url: str | None = None
+
+class GetAdmin(BaseModel):
+    name: str
+    pos_line_1: str
+    pos_line_2: str | None = None
+    orgs_name: str
+    about: str | None = None
+    resume_url: str | None = None
+
+    model_config = ConfigDict(from_attributes=True)
