08bd39c10d201c17bdf98d851fa915508363184b
Shreyash-Arya06 <shreyash.arya.iit.bhu@gmail.com>
2026-03-17 19:37:29 +0530

    Added cors origin to backend

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database.db import init_db
 from app.routes import admin
@@ -11,11 +12,25 @@ from app.routes import library
 
 app = FastAPI()
 
-# Database Setup (Create Tables on Startup)
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Database Setup
 @app.on_event("startup")
 async def on_startup():
     await init_db()
 
+# Routers
 app.include_router(admin.router)
 
 app.include_router(manage_admin.router)
